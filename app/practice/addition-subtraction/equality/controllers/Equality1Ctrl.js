7b4bf2c1785a054ec2c62528ae96e4d015c84cd1
Jason Figueroa <jason.figueroa2@gmail.com>
2017-12-19 13:34:02 -0600

    equality section is mostly functional

--- app/practice/addition-subtraction/equality/controllers/Equality1Ctrl.js
+++ app/practice/addition-subtraction/equality/controllers/Equality1Ctrl.js
@@ -1,7 +1,9 @@
 app.controller("Equality1Ctrl", function ($scope, $location) {
-  // $scope.msg = "works!";
+  
+  /*************/
+  /* Functions */
+  /*************/
 
-  // pasted in
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -12,7 +14,7 @@ app.controller("Equality1Ctrl", function ($scope, $location) {
     let a = getRandomInt(0, c);
     
     while(a > c - 2 || a <= 1) {
-      console.log(a, c)
+      // console.log(a, c)
       a = getRandomInt(0, c)
     }
   
@@ -40,13 +42,6 @@ app.controller("Equality1Ctrl", function ($scope, $location) {
   
     return set;
   }
-  // end pasted in
-
-  $scope.equality_set = [];
-
-  $scope.equality_set.push(buildEquality(15, 14));
-  $scope.equality_set.push(buildEquality(15));
-  $scope.equality_set.push(buildEquality(16, 15));
 
   function getCorrectAnswerIndex(arr) {
     for(let i = 0; i < arr.length; i++) {
@@ -63,42 +58,90 @@ app.controller("Equality1Ctrl", function ($scope, $location) {
     arr[indexB] = temp;
   };
 
-  const correctAnswerIndex = getCorrectAnswerIndex($scope.equality_set);
-  const newCorrectAnswerIndex = getRandomInt(0, 3);
+  $scope.nextQuestion = () => {
+    console.log('clicked next button');
 
-  console.log($scope.equality_set);
-  swapArrayElements($scope.equality_set, correctAnswerIndex, newCorrectAnswerIndex);
-  console.log($scope.equality_set);
+    reset_template();
+    messageSpan.attr('class', 'hidden');
+    nextBtn.addClass('hidden');
+
+    // if correct answer count === 10 congrats, log in db, redirect button
+
+    // else print next question and reset variables
+
+    // following is old
+    // $location.url('practice/addition-subtraction/equality/equality-2');
+  };
+
+  $scope.completeSection = () => {
+    $location.url('practice/child-dash');
+
+    // TODO 12-19-2017 mark this section as complete in the db for the active user
+    console.log('TODO 12-19-2017 mark this section as complete in the db for the active user');
+  };
+
+  $scope.showModalBtn = () => {
+    congratsModal.removeClass('hidden');
+  };
+
+  /********************/
+  /* End of Functions */
+  /********************/
+
+
+
+  /************************/
+  /* Initialize Variables */
+  /************************/
+
+  // the following is the correct answer
+  let answer = null;
+
+  const totalQuestions = 2;
+  let correctAnswerCount = 0;
   
-  // $scope.equality_set = {
-  //   equality_a: buildEquality(15, 14),
-  //   equality_b: buildEquality(15),
-  //   equality_c: buildEquality(16, 15)
-  // };
-
-  // console.log($scope.equality_set);
-
-  // $scope.answers = {
-  //   answer_a: `${$scope.equality_set.equality_a.equation_a} = ${$scope.equality_set.equality_a.equation_b}`,
-  //   answer_b: `${$scope.equality_set.equality_b.equation_a} = ${$scope.equality_set.equality_b.equation_b}`,
-  //   answer_c: `${$scope.equality_set.equality_c.equation_a} = ${$scope.equality_set.equality_c.equation_b}`
-  // };
-
-  // console.log($scope.answers)
-
-  $scope.userAnswer = '';
-  const answer = `${$scope.equality_set[newCorrectAnswerIndex].equation_a} = ${$scope.equality_set[newCorrectAnswerIndex].equation_b}`;
+  const correctAnswerIndex = 0;
+  // const correctAnswerIndex = getCorrectAnswerIndex($scope.equality_set);
+  
   const wrongAnswer = "Sorry that is incorrect, please try agin.";
   const correctAnswer = "That's correct, awesome job!"
+  
+  // may put this in the loop
   $scope.message = null;
-
+  
   // target elements
   const messageSpan = angular.element(document.querySelector('#message'));
   const nextBtn = angular.element(document.querySelector('#nextBtn'));
-  
+
+  const showModalBtn = angular.element(document.querySelector('#showModalBtn'));
+
+  // modal variables
+  const completeSectionBtn = angular.element(document.querySelector('#completeSectionBtn'));
+  const congratsModal = angular.element(document.querySelector('#congratsModal'));
+
+  /****************************/
+  /* End Initialize Variables */
+  /****************************/
+
+  // redirect will have to go in here
+  // also increment correctAnswerCount in here
   $scope.checkAnswer = () => {
+    
     messageSpan.attr('class', 'hidden');
     if($scope.userAnswer === answer) {
+      // following is new
+      correctAnswerCount++;
+      if(correctAnswerCount >= totalQuestions) {
+        console.log('section complete, should redirect here');
+        // display complete section button
+        showModalBtn.removeClass('hidden');
+        // redirect here
+      } else {
+        console.log(`completed ${correctAnswerCount} of ${totalQuestions} questions`);
+        // display next button
+        nextBtn.removeClass('hidden');
+      }
+      
       // change color of message span to green
       messageSpan.addClass('green');
       // bold text
@@ -107,8 +150,7 @@ app.controller("Equality1Ctrl", function ($scope, $location) {
       $scope.message = correctAnswer;
       // show message
       messageSpan.removeClass('hidden');
-      // display next button
-      nextBtn.removeClass('hidden');
+
     } else {
       // change color of message span to red
       messageSpan.addClass('red');
@@ -121,7 +163,52 @@ app.controller("Equality1Ctrl", function ($scope, $location) {
     }
   }
 
-  $scope.nextQuestion = () => {
-    $location.url('practice/addition-subtraction/equality/equality-2');
-  };
+  // $scope.completeSection = () => {
+  //   // display congratulatory modal
+  //   congratsModal.removeClass('hidden');
+
+  //   // in modal present button to redirect to list of available concepts to practice
+  // };
+
+  /**************/
+  /* Start Loop */
+  /**************/
+
+  function reset_template() {
+    // correct answer is reset to null
+    answer = null;
+    $scope.equality_set = [];
+  
+    const a = getRandomInt(8, 19)
+    const b = a + getRandomInt(1, 3)
+    const c = a - getRandomInt(1, 3)
+  
+    // buildEquality(a)
+    // buildEquality(a, b)
+    // buildEquality(c, a)
+    $scope.equality_set.push(buildEquality(a));
+    $scope.equality_set.push(buildEquality(a, b));
+    $scope.equality_set.push(buildEquality(c, a));
+  
+    const newCorrectAnswerIndex = getRandomInt(0, 3);
+    
+    // console.log($scope.equality_set);
+    swapArrayElements($scope.equality_set, correctAnswerIndex, newCorrectAnswerIndex);
+    // console.log($scope.equality_set);
+    
+    $scope.userAnswer = '';
+    answer = `${$scope.equality_set[newCorrectAnswerIndex].equation_a} = ${$scope.equality_set[newCorrectAnswerIndex].equation_b}`;
+  }
+
+  reset_template();
+
+  /************/
+  /* End Loop */
+  /************/
+
+  // following is old
+  // $scope.equality_set.push(buildEquality(15, 14));
+  // $scope.equality_set.push(buildEquality(15));
+  // $scope.equality_set.push(buildEquality(16, 15));  
+
 });
